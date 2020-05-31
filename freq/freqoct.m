a7fc0bbc08b0eda0f7fc9fa092f70e583f7c1559
mariotte <theo.mariotte@gmail.com>
2020-05-31 16:51:10 +0200

    DONE freqoct

--- freq/freqoct.m
+++ freq/freqoct.m
@@ -5,22 +5,44 @@ function freq = freqoct(noct,fmin,fmax,side_freq_fl)
 %   => 1/3 octave band etc...
 %   - fmin : minimum frequency
 %   - fmax : maximum frequency
-%   - side_freq_fl : include band limit frequencies in freq
+%   - side_freq_fl : include band limit frequencies in freq (usefull for
+%   band filter design)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+if nargin < 4, side_freq_fl = []; end
+if nargin < 3, fmax = []; end
+if nargin < 2, fmin = []; end
+if nargin < 1, noct = []; end
+
+% default : 1/1 octave band decomposition between 63 Hz and 8000 Hz without
+% limit frequencies
+if isempty(side_freq_fl), side_freq_fl = 0; end
+if isempty(fmax), fmax = 8000.; end
+if isempty(fmin), fmin = 62.; end
+if isempty(noct), noct = 1; end
+
 fref = 1000.;
-dk = 1:15;
+% TODO : this can be upgraded to be automated
+dk = 1:16;
 lowfact = 2.^(-dk./noct);
 upfact = 2.^(dk./noct);
 
 freq_inf = fref .* lowfact;
 freq_sup = fref .* upfact;
 
-
 freq_inf = freq_inf(freq_inf >= fmin);
 freq_sup = freq_sup(freq_sup <= fmax);
 
 freq = [freq_inf(end:-1:1) fref freq_sup];
+freq = shiftdim(freq);
+
+if side_freq_fl
+   
+    freq_inf = freq.* 2^(-1/2/noct);
+    freq_sup = freq.* 2^(+1/2/noct);
+    freq = [freq_inf freq freq_sup];
+    
+end
 
 
 end
--- freq/freqoct_test.m
+++ freq/freqoct_test.m
@@ -3,7 +3,20 @@ clear; clc; close all;
 
 fmin = 60;
 fmax = 10000;
-n = 3;
+n = 1;
 fl = 0;
 
+fprintf('No limit bands : \n');
 f = freqoct(n,fmin,fmax,fl);
+for k = 1 : length(f)
+   fprintf('Band %d : f = %6.4e\n',k,f(k)); 
+end
+
+% include limit frequencies
+fl = 1;
+f = freqoct(n,fmin,fmax,fl);
+fprintf('Include limit bands : \n');
+for k = 1 : length(f)
+   fprintf('Band %d | f_inf = %6.4e | f_c = %6.4e | f_sup = %6.4e \n',...
+       k,f(k,1),f(k,2),f(k,3)); 
+end
