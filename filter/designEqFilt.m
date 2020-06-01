dc07ba7161f3f9dd45781be9b999907e4dac0af3
mariotte <theo.mariotte@gmail.com>
2020-06-01 20:01:28 +0200

    Energyfl ok + test intégration bandes latérales (WIP)

--- filter/designEqFilt.m
+++ filter/designEqFilt.m
@@ -1,68 +1,112 @@
-function filterBank = designEqFilt(filtParam)
+function filterBank = designEqFilt(param)
 %% Banc de filtres passe-bande - décomposition en 1/n d'octave
 % 
-% filtParam : 
+% param : 
+%   - 'type' : type of filter 
+%              (1) Butterworth
+%              (2) Elliptic
+%              (3) Chebychev I
+%              (4) Chebychev II
+%   - 'rp' : pass band ripple (dB)
+%   - 'rs' : stop band attenuation (dB)
 %   - 'fmin' : lowest band center frequency
 %   - 'fmax' : 'highest band center frequency
 %   - 'fs'   : sampling rate
 %   - 'noct' : band decomposition (e.g. noct = 3 means 1/3 octave bands)
+%   - 'lateralBands' : include or not Low pass and High pass filters for
+%   lowest and highest bands
+%              (0) no lateral bands
+%              (1) add them
 %   - 'energyfl' : double filtering to conserve energy (useful for
 %   equalization)
 %   - 'order' : filter order
 %   - plotfl : (0) no plot ; (1) plot frequency response
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Matlab tools/filter
 
-if ~isfield(filtParam,'fmin'), filtParam.fmin = []; end
-if ~isfield(filtParam,'fmax'), filtParam.fmax = []; end
-if ~isfield(filtParam,'fs'), filtParam.fmin = []; end
-if ~isfield(filtParam,'noct'), filtParam.noct = []; end
-if ~isfield(filtParam,'order'), filtParam.order = 6; end
-if ~isfield(filtParam,'energyfl'), filtParam.energyfl = 0; end
-if ~isfield(filtParam,'plotfl'), filtParam.plotfl = 0; end
+if ~isfield(param,'type'), param.type = []; end
+if ~isfield(param,'rs'), param.rs = 40.; end
+if ~isfield(param,'rp'), param.rp = 1.; end
+if ~isfield(param,'fmin'), param.fmin = []; end
+if ~isfield(param,'fmax'), param.fmax = []; end
+if ~isfield(param,'fs'), param.fmin = []; end
+if ~isfield(param,'noct'), param.noct = []; end
+if ~isfield(param,'order'), param.order = 6; end
+if ~isfield(param,'lateralBands'), param.lateralBands = 0; end
+if ~isfield(param,'energyfl'), param.energyfl = 0; end
+if ~isfield(param,'plotfl'), param.plotfl = 0; end
 
-if isempty(filtParam.fs), error('Sampling rate should be specified !');end
-if isempty(filtParam.fmin)
-    filtParam.fmin = 60.;
+if isempty(param.fs), error('Sampling rate should be specified !');end
+if isempty(param.fmin)
+    param.fmin = 60.;
     warning('No low frequency limit. f = %4.2e used as default.',...
         paramFilt.fmin);
 end
-if isempty(filtParam.fmax)
-    filtParam.fmax = 8000.;
+if isempty(param.fmax)
+    param.fmax = 8000.;
     warning('No high frequency limit. f = %4.2e used as default.',...
         paramFilt.fmax);
 end
 
-if isempty(filtParam.noct)
-    filtParam.noct = 1;
+if isempty(param.noct)
+    param.noct = 1;
     warning('No octave band factor defined. noct = %d used as default.',...
         paramFilt.noct);
 end
+if isempty(param.type)
+    param.type = 1;
+    warning('No filter type found. Default = Butterworth');
+end
 
-if filtParam.plotfl
+if param.plotfl
     hh = figure('Name','Filter bank response');
     hold on;
     Hsum = 0;
     nbfreq = 2^15;
 end
 
+% In case of LP/HP filters for lateral bands
+lateral = param.lateralBands;
+if lateral
+    param.fmin = param.fmin *2^(-1/param.noct);
+    param.fmax = param.fmax *2^(1/param.noct);
+end
 
 % center and cutoff frequencies of each filter
-freq = freqoct(filtParam.noct,filtParam.fmin,filtParam.fmax,1);
+freq = freqoct(param.noct,param.fmin,param.fmax,1);
 % check Nyquist condition
-idx_keep = freq(:,3) <= filtParam.fs/2;
+idx_keep = freq(:,3) <= param.fs/2;
 freq = freq(idx_keep,:);
 nbfilt = size(freq,1);
 
 for ifilt = 1 : nbfilt
+    
+    if lateral==1 && ifilt==1
+        cut_type = 'low';
+        Ws = freq(ifilt,3);
+    elseif lateral == 1 && ifilt == nbfilt        
+        cut_type = 'high';
+        Ws = freq(ifilt,1);
+    else
+        cut_type = 'bandpass';
+        Ws = [freq(ifilt,1) freq(ifilt,3)];
+    end
+    
     filterBank(ifilt).fc = freq(ifilt,2);
-    Ws = [freq(ifilt,1) freq(ifilt,3)];
-    Wn = Ws / (filtParam.fs/2);
-    sos = getFilterCoef(1,filtParam.order,Wn, filtParam.energyfl);
+    Wn = Ws / (param.fs/2);
+    sos = getFilterCoef(param.type,param.order,Wn,...
+        param.rp,...
+        param.rs,...
+        cut_type);
     
     filterBank(ifilt).sos = sos; 
+    filterBank(ifilt).energyfl = param.energyfl;
     
-    if filtParam.plotfl               
-        [H,ff] = freqz(sos,nbfreq,filtParam.fs);
+    if param.plotfl               
+        [H,ff] = freqz(sos,nbfreq,param.fs);
+        if param.energyfl
+            H = H.*H;
+        end
         Hsum = Hsum + H;
         Hdb = 20*log10(abs(H));
         
@@ -72,14 +116,14 @@ for ifilt = 1 : nbfilt
     
 end
 
-if filtParam.plotfl
+if param.plotfl
     Hsum_db = 20*log10(abs(Hsum));
     plot(ff,Hsum_db,'linestyle','-','linewidth',2);
     grid on
     hold off
     xlabel('Frequency [Hz]')
     ylabel('Magnitude [dB]');
-    set(gca,'xlim',[freq(1,2)*2^(-1/filtParam.noct) filtParam.fs/2]);
+    set(gca,'xlim',[freq(1,2)*2^(-1/param.noct) param.fs/2]);
     set(gca,'xScale','log','xTick',freq(:,2));
     set(gca,'ylim',[-80 10])
 end
@@ -88,28 +132,45 @@ end
 
 end
 
-function [sos] = getFilterCoef(type,order,Wn,energyfl,rp,rs)
-
-if nargin < 6, rs = []; end
-if nargin < 5, rp = []; end
+function [sos] = getFilterCoef(type,order,Wn,rp,rs,cut)
 
 if type > 4, type = 1; end
 
 switch(type)
     % Butterworth
     case 1
-        [z,p,k] = butter(order,Wn,'bandpass');
-        sos = zp2sos(z,p,k);
+        if isempty(order)
+            error('No order found !');
+            % TODO : get order from frequencies
+            [order,Wn] = buttord(Wn,Wn,rp,rs);
+        end
+        [z,p,k] = butter(order,Wn,cut);  
+        
+    % elliptic
     case 2
+        if isempty(order)
+            error('No order found !');
+            [order,Wn] = ellipord(Wn(1),Wn(2),rp,rs);
+        end
+        [z,p,k] = ellip(order,rp,rs,Wn,cut);
         
+    % Cheby I
     case 3
+        if isempty(order)
+            error('No order found !');
+            [order,Wn] = cheb1ord(Wn(1),Wn(2),rp,rs);
+        end
+        [z,p,k] = cheby1(order,rp,Wn,cut);
         
+    % Cheby II
     case 4
-        
+        if isempty(order)
+            error('No order found !');
+            [order,Wn] = cheb2ord(Wn(1),Wn(2),rp,rs);
+        end
+        [z,p,k] = cheby2(order,rp,Wn,cut);
 end
+sos = zp2sos(z,p,k);
 
-if energyfl
-    sos = sos.*sos;
-end
 
 end
